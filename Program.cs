e334d7aa454129ae44a33116d6ccf299b44df37b
cwule <cleuze@nakamir.com>
2022-11-08 16:43:06 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using StereoKit;
+using System;
+
+namespace SKTagalong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize StereoKit
+            SKSettings settings = new SKSettings
+            {
+                appName = "SKTagalong",
+                assetsFolder = "Assets",
+            };
+            if (!SK.Initialize(settings))
+                Environment.Exit(1);
+
+            Pose menuPose;
+
+            // The Tagalong class including tagalong and billboard functionality
+            Tagalong _tagalong = new Tagalong();
+
+            SK.AddStepper(_tagalong);
+
+            // Core application loop
+            while (SK.Step(() =>
+            {
+                menuPose = _tagalong.ObjPose;
+
+                UI.WindowBegin("Tagalong Window", ref menuPose, UIWin.Body);
+
+                UI.Label("Content Header");
+                UI.HSeparator();
+                UI.Text("This window should always follow you and face you!", TextAlign.TopCenter);
+
+                UI.WindowEnd();
+
+            })) ;
+            SK.Shutdown();
+        }
+    }
+}
---
+++ Tagalong.cs
@@ -0,0 +1,78 @@
+﻿using StereoKit;
+using StereoKit.Framework;
+
+/// Billboard and Tagalong
+/// Script calculates the pose of an object so that it follows the user and always faces the user
+/// 
+
+namespace SKTagalong
+{
+    internal class Tagalong : IStepper
+    {
+        // the current pose of the object
+        private Pose _objPose = Pose.Identity;
+
+        // the desired pose of the object based on the head pose
+        private Pose _targetPose = Pose.Identity;
+
+        private bool _isLerping = false;
+
+        // the desired distance of the object on front of the user
+        private float _distance = 0.5f;
+        public bool Enabled => true;
+
+        public bool Initialize()
+        {
+            Matrix head = Input.Head.ToMatrix();
+
+            // the object is currently always set to be _distance meters straight in front of the user
+            Vec3 initPosition = head.Translation + head.Pose.Forward * _distance;
+            Quat initRotation = Quat.LookAt(initPosition, head.Translation);
+
+            _objPose = Matrix.TR(initPosition, initRotation).Pose;
+            _targetPose = Matrix.TR(initPosition, initRotation).Pose;
+
+            return true;
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public void Step()
+        {
+            Matrix head = Input.Head.ToMatrix();
+
+            // Tagalong, move along with user
+            if (_isLerping)
+            {
+                _objPose.position = Vec3.Lerp(_objPose.position, _targetPose.position, 0.1f);
+                if (_objPose.position.InRadius(_targetPose.position, 0.001f))
+                {
+                    _isLerping = false;
+                }
+            }
+            else if (!_objPose.position.InRadius(_targetPose.position, 0.3f))
+            {
+                _isLerping = true;
+            }
+            else
+            {
+                SetTargetPose(head);
+            }
+
+            // Billboarding, always face user
+            Quat objRotation = Quat.LookAt(_objPose.position, head.Translation);
+
+            _objPose = Matrix.TR(_objPose.position, objRotation).Pose;
+        }
+
+        public Pose ObjPose => _objPose;
+
+        // set the new world position where the object should be
+        private void SetTargetPose(Matrix headPose)
+        {
+            _targetPose.position = headPose.Translation + headPose.Pose.Forward * _distance;
+        }
+    }
+}
