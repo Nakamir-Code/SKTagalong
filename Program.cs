693c49e2d842f0cd8d7b71a5b807aae958eb739a
Austin Hale <haleau@live.unc.edu>
2022-11-09 15:11:48 -0500

    feat: option to place an xy-offset on the window

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ namespace SKTagalong
 {
     using StereoKit;
     using System;
-    using static System.Net.Mime.MediaTypeNames;
 
     internal class Program
     {
@@ -21,16 +20,29 @@ namespace SKTagalong
             Vec3 targetPos = Vec3.Zero;
             Vec3 nextTargetPos = Vec3.Zero;
 
+            Pose offsetMenuPose = Pose.Identity;
+            Vec3 offsetTargetPos = Vec3.Zero;
+            Vec3 offsetNextTargetPos = Vec3.Zero;
+            Vec2 offsetPos = V.XY(15, -10) * U.cm;
+
             // Core application loop
             while (SK.Step(() =>
             {
                 UIExtension.TagAlongBegin(ref menuPose, ref targetPos, ref nextTargetPos);
                 UI.WindowBegin("Tagalong Window", ref menuPose, UIWin.Body, UIMove.None);
-                UI.Label("Tagalong & Billboard");
+                UI.Label("Tagalong");
                 UI.HSeparator();
                 UI.Text("This window should always follow you and face you!", TextAlign.TopCenter);
                 UI.WindowEnd();
                 UIExtension.TagAlongEnd(ref menuPose, ref targetPos, ref nextTargetPos);
+
+                UIExtension.TagAlongBegin(ref offsetMenuPose, ref offsetTargetPos, ref offsetNextTargetPos, offsetPos);
+                UI.WindowBegin("Tagalong Window Offset", ref offsetMenuPose, UIWin.Body, UIMove.None);
+                UI.Label("Tagalong Offset");
+                UI.HSeparator();
+                UI.Text("This window uses an XY offset!", TextAlign.TopCenter);
+                UI.WindowEnd();
+                UIExtension.TagAlongEnd(ref offsetMenuPose, ref offsetTargetPos, ref offsetNextTargetPos);
             })) ;
             SK.Shutdown();
         }
--- Tagalong.cs
+++ Tagalong.cs
@@ -20,6 +20,20 @@ namespace SKTagalong
             pose.orientation = Quat.LookAt(pose.position, head.position);
         }
 
+        public static void TagAlongBegin(ref Pose pose, ref Vec3 targetPos, ref Vec3 nextTargetPos, Vec2 offset, float forwardDistance = 0.5f, float lerpBlend = 0.05f)
+        {
+            Pose head = Input.Head;
+
+            // The next target position will always be in front of the user.
+            nextTargetPos = head.position + head.Forward * forwardDistance + head.Right * offset.x + head.Up * offset.y;
+
+            // Tagalong, move the window along with user at the previous target.
+            pose.position = Vec3.Lerp(pose.position, targetPos, lerpBlend);
+
+            // Billboarding always faces the user.
+            pose.orientation = Quat.LookAt(pose.position, head.position);
+        }
+
         public static void TagAlongEnd(ref Pose pose, ref Vec3 targetPos, ref Vec3 nextTargetPos, float collisionRadius = 0.15f)
         {
             // Set the new target position for when it is outside the user's FOV
@@ -33,7 +47,7 @@ namespace SKTagalong
             Material transparentMat = Material.Default.Copy();
             transparentMat.Transparency = Transparency.Add;
             Mesh.Sphere.Draw(transparentMat, Matrix.TS(targetPos, collisionRadius), Color.Hex(0x0000FFFF));
-            Mesh.Sphere.Draw(transparentMat, Matrix.TS(nextTargetPos, collisionRadius), Color.Hex(0xFF0000FF));
+            Mesh.Sphere.Draw(transparentMat, Matrix.TS(nextTargetPos, collisionRadius), Color.Hex(0xFF0000FF));            
 #endif
         }
     }
