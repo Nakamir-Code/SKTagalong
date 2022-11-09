5c747877e3adc1493e7afd1ba39fadf0cf21a5cf
Austin Hale <haleau@live.unc.edu>
2022-11-09 13:41:54 -0500

    refactor: separate from windowbegin extension method with its own TagAlongBegin.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ namespace SKTagalong
 {
     using StereoKit;
     using System;
+    using static System.Net.Mime.MediaTypeNames;
 
     internal class Program
     {
@@ -18,15 +19,18 @@ namespace SKTagalong
 
             Pose menuPose = Pose.Identity;
             Vec3 targetPos = Vec3.Zero;
+            Vec3 nextTargetPos = Vec3.Zero;
 
             // Core application loop
             while (SK.Step(() =>
             {
-                UIExtension.WindowBegin("Tagalong Window", ref menuPose, ref targetPos, UIWin.Body);
+                UIExtension.TagAlongBegin(ref menuPose, ref targetPos, ref nextTargetPos);
+                UI.WindowBegin("Tagalong Window", ref menuPose, UIWin.Body, UIMove.None);
                 UI.Label("Tagalong & Billboard");
                 UI.HSeparator();
                 UI.Text("This window should always follow you and face you!", TextAlign.TopCenter);
                 UI.WindowEnd();
+                UIExtension.TagAlongEnd(ref menuPose, ref targetPos, ref nextTargetPos);
             })) ;
             SK.Shutdown();
         }
--- Tagalong.cs
+++ Tagalong.cs
@@ -6,43 +6,35 @@ namespace SKTagalong
 
     public static class UIExtension
     {
-        public static void WindowBegin(string text, ref Pose pose, ref Vec3 targetPos, Vec2 size, UIWin windowType = UIWin.Normal, float forwardDistance = 0.5f, float collisionRadius = 0.1f, float lerpBlend = 0.05f)
+        public static void TagAlongBegin(ref Pose pose, ref Vec3 targetPos, ref Vec3 nextTargetPos, float forwardDistance = 0.5f, float lerpBlend = 0.05f)
         {
             Pose head = Input.Head;
 
             // The next target position will always be in front of the user.
-            Vec3 nextTargetPose = head.position + head.Forward * forwardDistance;
+            nextTargetPos = head.position + head.Forward * forwardDistance;
 
             // Tagalong, move the window along with user at the previous target.
             pose.position = Vec3.Lerp(pose.position, targetPos, lerpBlend);
 
-            // Set the new target position for when it is outside the user's FOV
-            // so that we lerp only when the new window position is out of range.
-            if (!pose.position.InRadius(nextTargetPose, collisionRadius))
-            {
-                targetPos = nextTargetPose;
-            }
-
             // Billboarding always faces the user.
             pose.orientation = Quat.LookAt(pose.position, head.position);
-
-#if DEBUG
-            Material blueTransparent = Material.Default.Copy();
-            blueTransparent.Transparency = Transparency.Add;
-            blueTransparent["color"] = Color.Hex(0x0000FFFF);
-
-            Material redTransparent = Material.Default.Copy();
-            redTransparent.Transparency = Transparency.Add;
-            redTransparent["color"] = Color.Hex(0xFF0000FF);
-
-            Mesh.Sphere.Draw(blueTransparent, Matrix.TS(targetPos, collisionRadius));
-            Mesh.Sphere.Draw(redTransparent, Matrix.TS(nextTargetPose, collisionRadius));
-#endif
-
-            UI.WindowBegin(text, ref pose, size, windowType, UIMove.None);
         }
 
-        public static void WindowBegin(string text, ref Pose pose, ref Vec3 targetPos, UIWin windowType = UIWin.Normal, float forwardDistance = 0.5f, float collisionRadius = 0.1f, float lerpBlend = 0.05f)
-            => WindowBegin(text, ref pose, ref targetPos, Vec2.Zero, windowType, forwardDistance, collisionRadius, lerpBlend);
+        public static void TagAlongEnd(ref Pose pose, ref Vec3 targetPos, ref Vec3 nextTargetPos, float collisionRadius = 0.15f)
+        {
+            // Set the new target position for when it is outside the user's FOV
+            // so that we lerp only when the new window position is out of range.
+            if (!pose.position.InRadius(nextTargetPos, collisionRadius))
+            {
+                targetPos = nextTargetPos;
+            }
+
+#if DEBUG
+            Material transparentMat = Material.Default.Copy();
+            transparentMat.Transparency = Transparency.Add;
+            Mesh.Sphere.Draw(transparentMat, Matrix.TS(targetPos, collisionRadius), Color.Hex(0x0000FFFF));
+            Mesh.Sphere.Draw(transparentMat, Matrix.TS(nextTargetPos, collisionRadius), Color.Hex(0xFF0000FF));
+#endif
+        }
     }
 }
