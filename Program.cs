c940ff4ae264c67bcda7843b38dc0f54ba374730
Austin Hale <haleau@live.unc.edu>
2022-11-10 12:45:15 -0500

    fix: specify starting positions

--- Program.cs
+++ Program.cs
@@ -16,19 +16,26 @@ namespace SKTagalong
             if (!SK.Initialize(settings))
                 Environment.Exit(1);
 
-            Pose menuPose = Pose.Identity;
-            Vec3 targetPos = Vec3.Zero;
-            Vec3 nextTargetPos = Vec3.Zero;
+            Pose head = Input.Head;
+            float forwardDistance = 0.5f;
 
-            Pose offsetMenuPose = Pose.Identity;
-            Vec3 offsetTargetPos = Vec3.Zero;
-            Vec3 offsetNextTargetPos = Vec3.Zero;
-            Vec2 offsetPos = V.XY(15, -10) * U.cm;
+            // Set the starting pose of the menu
+            Vec3 menuPosition = head.position + head.Forward * forwardDistance;
+            Pose menuPose = new Pose(menuPosition, Quat.LookAt(menuPosition, head.position));
+            Vec3 targetPos = menuPosition;
+            Vec3 nextTargetPos = Vec3.Zero; // ignore, set inside TagAlong
 
-            // Core application loop
-            while (SK.Step(() =>
+            // Set the starting pose of the menu with an offset
+            Vec2 offset = V.XY(15, -10) * U.cm;
+            Vec3 offsetMenuPosition = head.position + head.Forward * forwardDistance + head.Right * offset.x + head.Up * offset.y;
+            Pose offsetMenuPose = new Pose(offsetMenuPosition, Quat.LookAt(offsetMenuPosition, head.position));
+            Vec3 offsetTargetPos = offsetMenuPosition;
+            Vec3 offsetNextTargetPos = Vec3.Zero; // ignore, set inside TagAlong
+
+            // Core application
+            SK.Run(() =>
             {
-                UIExtension.TagAlongBegin(ref menuPose, ref targetPos, ref nextTargetPos);
+                UIExtension.TagAlongBegin(ref menuPose, ref targetPos, ref nextTargetPos, forwardDistance);
                 UI.WindowBegin("Tagalong Window", ref menuPose, UIWin.Body, UIMove.None);
                 UI.Label("Tagalong");
                 UI.HSeparator();
@@ -36,15 +43,14 @@ namespace SKTagalong
                 UI.WindowEnd();
                 UIExtension.TagAlongEnd(ref menuPose, ref targetPos, ref nextTargetPos);
 
-                UIExtension.TagAlongBegin(ref offsetMenuPose, ref offsetTargetPos, ref offsetNextTargetPos, offsetPos);
+                UIExtension.TagAlongBegin(ref offsetMenuPose, ref offsetTargetPos, ref offsetNextTargetPos, offset, forwardDistance);
                 UI.WindowBegin("Tagalong Window Offset", ref offsetMenuPose, UIWin.Body, UIMove.None);
                 UI.Label("Tagalong Offset");
                 UI.HSeparator();
                 UI.Text("This window uses an XY offset!", TextAlign.TopCenter);
                 UI.WindowEnd();
                 UIExtension.TagAlongEnd(ref offsetMenuPose, ref offsetTargetPos, ref offsetNextTargetPos);
-            })) ;
-            SK.Shutdown();
+            });
         }
     }
 }
